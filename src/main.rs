bb8aa6a7f8dad3e493fccc3b99528d0ab3a4d77e
Overflow922 <genius922@yandex.ru>
2022-07-08 14:05:06 +0300

    Implementing MyBox smart ptr with dereference

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod cons_list;
+mod my_box;
 
 use crate::cons_list::List;
+use crate::my_box::MyBox;
 
 fn main() {
     let _list = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let name = MyBox::new(String::from("Rust"));
+    hello(&name);
+}
+
+fn hello(name: &str) {
+    println!("hello, {}", name);
 }
